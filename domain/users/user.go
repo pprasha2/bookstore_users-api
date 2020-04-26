fa53a1af42157d8a8450240efc86bdff1820846d
prashant <prashant.sharma112@gmail.com>
2020-04-26 14:20:54 +0530

    before adding persistence layer

--- domain/users/user.go
+++ domain/users/user.go
@@ -1,5 +1,11 @@
 package users
 
+import (
+	"strings"
+
+	"github.com/pprasha2/bookstore_users-api/utils/errors"
+)
+
 //User structure
 type User struct {
 	Id          int64  `json:"id"`
@@ -7,4 +13,13 @@ type User struct {
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
 	DateCreated string `json:"date_created"`
+}
+
+//Validate validates the checks before processing further
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("Invalid Email address")
+	}
+	return nil
 }
--- services/users_service.go
+++ services/users_service.go
@@ -1,18 +1,22 @@
 package services
 
 import (
-	"net/http"
-
 	"github.com/pprasha2/bookstore_users-api/domain/users"
 	"github.com/pprasha2/bookstore_users-api/utils/errors"
 )
 
 //CreateUser - creates a user
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
-	//	return &user, nil
-	return nil, &errors.RestErr{
-		Message: "Internal server error",
-		Status:  http.StatusInternalServerError,
-		Error:   "Internal Server Error",
+
+	/*
+		return nil, &errors.RestErr{
+			Message: "Internal server error",
+			Status:  http.StatusInternalServerError,
+			Error:   "Internal Server Error",
+		}
+	*/
+	if err := user.Validate(); err != nil {
+		return &user, nil
 	}
+	return &user, nil
 }
